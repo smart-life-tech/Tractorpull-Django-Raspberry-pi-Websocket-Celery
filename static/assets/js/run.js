31d9e1e10182d6f39116c43e38f9fbd93c009af9
christolightTech <58917014+smart-life-tech@users.noreply.github.com>
2025-02-23 05:52:14 +0100

    Update run.js
    if (values[1].replace( /[^\d\.]*/g, '').length == 0)
                //         break;

--- static/assets/js/run.js
+++ static/assets/js/run.js
@@ -52,7 +52,8 @@ function connect() {
             
             let values = message.split(">");
             if (values.length == 1 || !values) break;
-             $('#competitor-running-distance').html(parseFloat(values[1].replace( /[^\d\.]*/g, '')));
+            if (values[1].replace( /[^\d\.]*/g, '').length == 0)  break;
+            $('#competitor-running-distance').html(parseFloat(values[1].replace( /[^\d\.]*/g, '')));
             $('#competitor-running-speed').html(parseFloat(values[3].replace(/[^\d\.]*/g, '')));
 
             let distance = parseFloat(values[1].replace(/[^\d\.]*/g, ''));
