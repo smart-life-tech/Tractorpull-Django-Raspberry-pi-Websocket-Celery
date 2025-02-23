7e84620d5644901b462f4fda35280c92b44814f6
smart-life-tech <christlightworld@gmail.com>
2025-02-23 21:21:58 +0100

    modified:   start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cd /home/pi/Desktop/Tractorpull-Django-Raspberry-pi-Websocket-Celery/
-cd /home/pi/Desktop/NEW GUI/New
+cd /home/pi/Desktop/NEW_GUI/New
 ./redis-stable/src/redis-server &
 #redis-server &
 
